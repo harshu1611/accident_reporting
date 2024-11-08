9e35a6bad2f18cfa0a96c1fe4a4246025b505678
Raj Motwani <rajmotwani38@gmail.com>
2024-11-08 17:24:25 +0530

    added login page(need fixes)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Redirect, Route } from 'react-router-dom';
+import { Redirect, Route } from "react-router-dom";
+import "./global.css";
 import {
   IonApp,
   IonIcon,
@@ -7,29 +8,29 @@ import {
   IonTabBar,
   IonTabButton,
   IonTabs,
-  setupIonicReact
-} from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+  setupIonicReact,
+} from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import { ellipse, square, triangle } from "ionicons/icons";
+import LoginPage from "./pages/Tab1";
+import Tab2 from "./pages/Tab2";
+import Tab3 from "./pages/Tab3";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /**
  * Ionic Dark Mode
@@ -40,10 +41,10 @@ import '@ionic/react/css/display.css';
 
 /* import '@ionic/react/css/palettes/dark.always.css'; */
 /* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
+import "@ionic/react/css/palettes/dark.system.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
 setupIonicReact();
 
@@ -53,7 +54,7 @@ const App: React.FC = () => (
       <IonTabs>
         <IonRouterOutlet>
           <Route exact path="/tab1">
-            <Tab1 />
+            <LoginPage />
           </Route>
           <Route exact path="/tab2">
             <Tab2 />
@@ -76,7 +77,7 @@ const App: React.FC = () => (
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonLabel className="text-red-500">Tab 3</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,25 +1,85 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import React, { useState } from "react";
+import {
+  IonContent,
+  IonPage,
+  IonInput,
+  IonButton,
+  IonIcon,
+  IonLabel,
+} from "@ionic/react";
+import { mailOutline, lockClosedOutline } from "ionicons/icons";
 
-const Tab1: React.FC = () => {
+const LoginPage: React.FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleLogin = () => {
+    // Implement login functionality here
+    console.log("Logging in with:", { email, password });
+  };
+
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+    <IonPage >
+      <IonContent className="flex items-center justify-center h-full bg-gray-100 ">
+        <div className="w-full  p-8 space-y-6 bg-white rounded-lg shadow-md max-w-screen-lg">
+          <h2 className="text-2xl font-bold text-center text-gray-800">
+            Login
+          </h2>
+
+          <div className="space-y-4 text-black">
+            <div className="flex flex-col">
+              <IonLabel className="text-gray-600">Email</IonLabel>
+              <div className="relative">
+                <IonIcon
+                  icon={mailOutline}
+                  className="absolute top-2 left-2 text-gray-400"
+                />
+                <IonInput
+                  type="email"
+                  value={email}
+                  onIonChange={(e) => setEmail(e.detail.value!)}
+                  placeholder="Enter your email"
+                  className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none"
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-col">
+              <IonLabel className="text-gray-600">Password</IonLabel>
+              <div className="relative">
+                <IonIcon
+                  icon={lockClosedOutline}
+                  className="absolute top-2 left-2 text-gray-400"
+                />
+                <IonInput
+                  type="password"
+                  value={password}
+                  onIonChange={(e) => setPassword(e.detail.value!)}
+                  placeholder="Enter your password"
+                  className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none"
+                />
+              </div>
+            </div>
+
+            <IonButton
+              expand="block"
+              onClick={handleLogin}
+              className="w-full py-2 mt-6 font-bold text-white bg-blue-600 rounded-md hover:bg-blue-700"
+            >
+              Login
+            </IonButton>
+
+            <p className="text-sm text-center text-gray-600">
+              Don’t have an account?{" "}
+              <a href="/signup" className="text-blue-600 hover:underline">
+                Sign Up
+              </a>
+            </p>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab1;
+export default LoginPage;
